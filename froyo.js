8964b7674506efc632863ef70443013331e09827
Mercede Chen <mercedechen@gmail.com>
2023-11-11 10:21:50 -0500

    Prompt user to input flavors and store input in a variable

---
+++ froyo.js
@@ -0,0 +1,8 @@
+// Prompt the user for their froyo flavor(s) order separated by commas.
+const userInputFlavors = window.prompt("To order, enter froyo flavors separated by commas: ", "vanilla, strawberry");
+
+// Stores user's order to a variable
+const orderFlavors = userInputFlavors;
+console.log(orderFlavors);
+
+
