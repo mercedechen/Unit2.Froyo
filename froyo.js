3e6cd74cf31520cf714249e8bf021534856134ca
Mercede Chen <mercedechen@gmail.com>
2023-11-11 21:18:57 -0500

    Convert user input string to array

--- froyo.js
+++ froyo.js
@@ -1,13 +1,31 @@
-// Prompt the user to enter froyo flavor(s) separated by commas
+// Prompt the user to input froyo flavor(s) separated by commas, input is a string
 const userInputFlavors = window.prompt("To order, enter froyo flavors separated by commas: ", "vanilla, strawberry");
+console.log(userInputFlavors);
 
-// Store users order to a variable
-const inputFlavors = userInputFlavors;
-console.log(inputFlavors);
+// Store user input to a variable
+const userFroyoOrder = userInputFlavors;
+console.log(userFroyoOrder);
+
+// Convert string to array
+const flavorsArray = userFroyoOrder.split(', ');
+console.log(flavorsArray);
 
 
+// function flavorsCount(items) {
+//   const flavors = {};
+//   for (let i = 0; i < items.length; i++) {
+//     if (flavors[items[i]]) {
+//       flavors[items[i]]++;
+//     } else {
+//       flavors[items[i]] = 1;
+//     }
+//   }
+//   return flavors;
+// }
+// console.log(flavorsCount(froyoFlavors));
 
-const froyo = {
-  flavors: ["vanilla", "strawberry", "coffee", "chocolate", "caramel", "mint"]
-}
-console.log(froyo);
+// const froyo = {
+//   flavors: ["vanilla", "strawberry", "coffee", "chocolate", "caramel", "mint"]
+// }
+// console.log(froyo);
+
